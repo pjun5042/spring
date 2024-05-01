49026d1662c1e95c13b9703607f073e5cbe60d92
user1 <user1@DESKTOP-9CFKG1O>
2024-05-01 11:41:13 +0900

    zz

--- ex01/src/main/java/com/kh/demo/controller/SampleController.java
+++ ex01/src/main/java/com/kh/demo/controller/SampleController.java
@@ -1,9 +1,16 @@
 package com.kh.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.demo.dto.SampleDTO;
 
 @Controller
 @RequestMapping("/sample/*")
@@ -32,7 +39,53 @@ public class SampleController {
 		System.out.println("age : "+age);
 	}
 	
+	@RequestMapping(value="basic2", method = {RequestMethod.GET,RequestMethod.POST})
+	public void basic2() {
+		System.out.println("========GET, POST 방식으로 요청========");
+	}
+	
+	//DTO 객체로 수집하기
+	@GetMapping("test01")
+	public void test01(SampleDTO dto) {
+		System.out.println(dto);
+	}
+	
+	//파라미터 명과 담을 변수명이 다른 경우 수집 방법(특정 이름의 파라미터로 날아오는 데이터 수집 방법)
+	@GetMapping("test02")
+	public void test02(@RequestParam("data1") String name,@RequestParam("data2") int age) {
+		System.out.println(name + " / " + age);
+	}
+	
+	//파라미터가 같은 이름으로 여러개 날라오는 경우 수집 방법
+	@GetMapping("test03")
+	public void test03(@RequestParam("data") String[] datas, Model model) {
+		model.addAttribute("datas",datas);
+	}
+	
+	@GetMapping("test04")
+	public void test04(@RequestParam("data") ArrayList<Integer> datas, Model model ) {
+		model.addAttribute("datas",datas);
+	}
+	
+	@GetMapping("test05")
+	public void test05() {
+		String str = null;
+		str.charAt(10);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
